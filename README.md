f159ed2020d22b795322a124dd3a3c85a099094e
singh1114 <ranvir.singh1114@gmail.com>
2017-11-18 01:57:30 +0530

    Add bot files
    * Add documentation on how to use

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # slackbot
 A simple slack bot
+
+
+## Installations
+
+### Using pip
+
+- Fork the repo
+
+`$ virtualenv slackbot`
+
+`$ cd slackbot`
+
+`$ source bin/activate`
+
+`$ pip install slackclient`
+
+`$ git clone https://github.com/YOUR_GITHUB_USERNAME/slackbot`
+
+- change YOUR_GITHUB_USERNAME by your username
+
+`$ cd slackbot`
+
+- Fill in the details of bot in `bot.py` in line _7_ and _8_
+
+`$ python anotherbot.py`
+
+- Now, in the message box of slack, refer to your bot like @Your_bot_name `timezone`.
+
+- Use the following format for timezone
+
+- if it is +0530 use +5.5
+- if it is -0550 use -5.5
+
+- The bot will send a message at 12;00 pm refering to you.
---
+++ command.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from datetime import timedelta
+import time
+
+class Command(object):
+    
+    def __init__(self):
+        self.commands = { 
+            "jump" : self.jump,
+            "help" : self.help,
+            "timezone" : self.timezone,
+        }
+ 
+    def handle_command(self, user, command):
+        import re
+        response = "<@" + user + ">: "
+     
+        if command in self.commands:
+            response += self.commands[command]()
+        elif re.match(r'^[+-]?\d+[.]?\d+?$', command):
+            utc_time = datetime.utcnow()
+            print utc_time
+
+            timezone_time = utc_time + timedelta(hours=float(command))
+            print timezone_time
+
+            utc_hour = utc_time.hour + utc_time.minute / 60 + utc_time.second / 3600
+
+            if(timezone_time.hour<12):
+                time_till_noon = 11 - timezone_time.hour
+                print time_till_noon
+                seconds_remaining = time_till_noon * 60 * 60 + (59-timezone_time.minute) * 60 + (59-timezone_time.second)
+            else:
+                time_till_noon = 35 - timezone_time.hour
+                seconds_remaining = time_till_noon * 60 * 60 - timezone_time.minute * 60 - timezone_time.second
+
+            print seconds_remaining
+
+            time.sleep(seconds_remaining)
+
+            response += "Hi!"
+        else:
+            response += "Sorry I don't understand the command: " + command + ". " + self.help()
+         
+        return response
+         
+    def jump(self):
+        return "Kris Kross will make you jump jump"
+
+    def timezone(self):
+        return "What is your timezone?"
+
+     
+    def help(self):
+        response = "Currently I support the following commands:\r\n"
+         
+        for command in self.commands:
+            response += command + "\r\n"
+             
+        return response
